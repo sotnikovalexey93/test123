2cb89cb0ec09c736b25ea7528f0f67a889c0b763
Alexey <sotnikovalexey93@mail.ru>
2023-01-11 22:04:50 +0300

    ИндентиФИКАТОР

---
+++ Example005_ConditionFL/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "алисочка" )
+{
+    Console.WriteLine("Ура Алисочка, я вас ЛЮБЛЮ!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
