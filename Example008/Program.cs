1c31d6faf541f57dc175f996ed3db59c05e33932
Alexey <sotnikovalexey93@mail.ru>
2023-01-15 18:44:39 +0300

    к сожалению не всё так просто(

---
+++ Example008/Program.cs
@@ -0,0 +1,25 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2> result) result = arg2;
+    if(arg3> result) result = arg3;
+    return result;
+}
+
+
+int a1 = 12;    
+int b1 = 22;
+int c1 = 2456;
+int a2 = 12;    
+int b2 = 68;
+int c2 = 8;
+int a3 = 56;    
+int b3 = 88888;
+int c3 = 133;
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);
---
+++ Praktika001/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Напишите день недели: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+if(number == 1){
+    Console.WriteLine("Понедельник");
+}
+else if(number == 2){
+    Console.WriteLine("Вторник");
+}
+else if(number == 3){
+    Console.WriteLine("Среда");
+}
+else if(number == 4){
+    Console.WriteLine("Четверг");
+}
+else if(number == 5){
+    Console.WriteLine("Пятница");
+}
+else if(number == 6){
+    Console.WriteLine("Суббота");
+}
+else if(number == 7){
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Такого дня нет");
+}
